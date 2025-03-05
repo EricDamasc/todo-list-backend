a6ea39acee8793cc30b34684281f3be42c89f63e
EricDamasc <ericdamascena17@hotmail.com>
2025-03-05 10:31:18 -0300

    Adiciona suporte ao CloudWatch para logging e ajusta a configuração do logger

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 import logging
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from app.routers import tasks
 from app.auth import auth
+from watchtower import CloudWatchLogHandler
 
 # Configuração do logger
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+# Configuração do CloudWatch
+cloudwatch_handler = CloudWatchLogHandler(
+    log_group=os.getenv("CLOUDWATCH_LOG_GROUP", "todo-list-backend-logs"),)
+logger.addHandler(cloudwatch_handler)
 
 app = FastAPI(
     title="To-Do List API",
@@ -23,6 +31,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.options("/{path:path}")
 async def preflight_request():
     logger.info("Preflight request received")
@@ -30,10 +39,12 @@ async def preflight_request():
         "message": "Preflight request successful"
     }
 
+
 @app.get("/", tags=["Home"])
 def root():
     logger.info("Root endpoint accessed")
     return {"message": "Bem-vindo à API de Tarefas!"}
+
 
 app.include_router(auth.router, prefix="/api", tags=["Autenticação"])
 app.include_router(tasks.router, prefix="/api", tags=["Tarefas"])
